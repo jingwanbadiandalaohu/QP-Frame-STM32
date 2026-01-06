09efbdfcccccef5feb96b28a22c20f9d3c7f9e5f
dylan <1437168566@qq.com>
2026-01-06 16:26:57 +0800

    工程适配keil-AC5编译链

---
+++ project/stm32h750vbt6/usr/inc/RTE_Components.h
@@ -0,0 +1,8 @@
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+// Minimal CMSIS component definitions for Keil AC5 builds.
+// Adjust if you enable more RTE components.
+#define CMSIS_device_header "stm32h750xx.h"
+
+#endif
