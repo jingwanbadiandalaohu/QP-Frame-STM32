296500fed5c034757b96f1acbaf1fab68d542075
dylan <1437168566@qq.com>
2026-05-11 11:16:36 +0800

    feat:MDK项目适配f103和h750，编译通过

---
+++ project/usr/inc/stm32f103rct6/RTE_Components.h
@@ -0,0 +1,17 @@
+/**
+ * @file    RTE_Components.h
+ * @author  Dylan
+ * @date    2026-05-11
+ * @brief   CMSIS 运行时环境组件配置
+ *
+ * @details 针对 STM32F103RCT6 工程的最小化 CMSIS 组件定义
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+// Minimal CMSIS component definitions for Keil AC5/AC6 builds.
+// Adjust if you enable more RTE components.
+#define CMSIS_device_header "stm32f1xx.h"
+
+#endif /* RTE_COMPONENTS_H */
